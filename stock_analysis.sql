3a96cef92ea73a9e67ec53c42cb1646b70526596
Erin Nathania Susanto <nathaniaerin@gmail.com>
2026-04-12 23:12:10 +0800

    Created 3 queries

--- stock_analysis.sql
+++ stock_analysis.sql
@@ -1,15 +1,7 @@
-SELECT 
-    Product_Name,
-    Category,
-    Stock_Quantity,
-    Reorder_Level,
-    Reorder_Quantity,
-    Supplier_Name,
-    Status
-FROM grocery_inventory.inventory
-WHERE Stock_Quantity < Reorder_Level
-ORDER BY (Reorder_Level - Stock_Quantity) DESC;
+#Checks understocked items
 
+
+#Checks overlying stock
 SELECT 
     Product_Name,
     Category,
@@ -23,3 +15,24 @@ FROM grocery_inventory.inventory
 WHERE Stock_Quantity > Sales_Volume
   AND Inventory_Turnover_Rate < 30
 ORDER BY Capital_Tied_Up DESC;
+
+#Consider now 2024 February - This query checks the most urgent expiry date
+SELECT 
+    Product_Name,
+    Product_ID,
+    Category,
+    Expiration_Date
+FROM grocery_inventory.inventory
+WHERE Expiration_Date < '2024-08-01' AND Stock_Quantity > 0
+ORDER BY Expiration_Date ASC;
+
+#This query checks for biggest potential loss
+SELECT 
+    Product_Name,
+    Product_ID,
+    Category,
+    Expiration_Date,
+    ROUND(Stock_Quantity * Unit_Price, 2) AS Waste_Loss
+FROM grocery_inventory.inventory
+WHERE Expiration_Date < '2024-08-01' AND Stock_Quantity > 0
+ORDER BY Waste_Loss DESC;
